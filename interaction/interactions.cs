3bcbf2a99cc159cced6bc25b8e93b21264325ea1
FiollDoll <86582596+FiollDoll@users.noreply.github.com>
2024-08-03 20:36:53 +0500

    location auto exti + switches in interactions

--- interaction/interactions.cs
+++ interaction/interactions.cs
@@ -6,79 +6,102 @@ public class interactions : MonoBehaviour
 {
     [SerializeField] private GameObject iconInteraction;
     [SerializeField] private allScripts scripts;
-    private string totalColliderName;
-    private string totalColliderMode;
+    private string totalColliderName, totalColliderMode, spawnName;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "dialog")
-            scripts.dialogsManager.ActivateDialog(other.gameObject.name);
-        else if (other.gameObject.tag == "interact" || other.gameObject.tag == "item")
+        switch (other.gameObject.tag)
         {
-            if (other.gameObject.GetComponent<extraInteraction>())
-            {
-                if (other.gameObject.GetComponent<extraInteraction>().stageInter == scripts.quests.totalStep)
+            case "dialog":
+                scripts.dialogsManager.ActivateDialog(other.gameObject.name);
+                break;
+            case "location":
+                char[] nameLocationChar = other.gameObject.name.ToCharArray();
+                totalColliderName = "";
+                totalColliderMode = "location";
+                spawnName = "";
+                for (int i = 0; i < nameLocationChar.Length; i++)
+                {
+                    if (i != (nameLocationChar.Length - 1))
+                        totalColliderName += nameLocationChar[i];
+                    else
+                        spawnName = nameLocationChar[i].ToString();
+                }
+                if (scripts.locations.GetLocation(totalColliderName).autoEnter && scripts.locations.totalLocation.autoEnter)
+                    scripts.locations.ActivateLocation(totalColliderName, spawnName);
+                else
+                    iconInteraction.gameObject.SetActive(true);
+                break;
+            case "interact":
+            case "item":
+                if (other.gameObject.GetComponent<extraInteraction>())
+                {
+                    if (other.gameObject.GetComponent<extraInteraction>().stageInter == scripts.quests.totalStep)
+                    {
+                        iconInteraction.gameObject.SetActive(true);
+                        totalColliderName = other.gameObject.name;
+                        totalColliderMode = other.gameObject.tag;
+                    }
+                }
+                else
                 {
                     iconInteraction.gameObject.SetActive(true);
                     totalColliderName = other.gameObject.name;
                     totalColliderMode = other.gameObject.tag;
                 }
-            }
-            else
-            {
-                iconInteraction.gameObject.SetActive(true);
-                totalColliderName = other.gameObject.name;
-                totalColliderMode = other.gameObject.tag;
-            }
-
-        }
-        else if (other.gameObject.tag == "location")
-        {
-            char[] nameLocationChar = other.gameObject.name.ToCharArray();
-            string nameLocation = "";
-            string spawnName = "";
-            for (int i = 0; i < nameLocationChar.Length; i++)
-            {
-                if (i != (nameLocationChar.Length - 1))
-                    nameLocation += nameLocationChar[i];
-                else
-                    spawnName = nameLocationChar[i].ToString();
-            }
-            scripts.locations.ActivateLocation(nameLocation, spawnName);
+                break;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.tag == "interact" || other.gameObject.tag == "item")
-            iconInteraction.gameObject.SetActive(false);
+        switch (other.gameObject.tag)
+        {
+            case "interact":
+            case "item":
+                iconInteraction.gameObject.SetActive(false);
+                break;
+            case "location":
+                iconInteraction.gameObject.SetActive(false);
+                break;
+        }
+        totalColliderName = "";
+        totalColliderMode = "";
+        spawnName = "";
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if (totalColliderMode != "item")
+            if (totalColliderName != "")
             {
-                if (!scripts.dialogsManager.dialogMenu.activeSelf)
-                    scripts.dialogsManager.ActivateDialog(totalColliderName);
-            }
-            else
-            {
-                Destroy(GameObject.Find(totalColliderName));
-                iconInteraction.gameObject.SetActive(false);
-                scripts.inventory.AddItem(totalColliderName);// Переделать потом на questItem
-            }
+                switch (totalColliderMode)
+                {
+                    case "item":
+                        Destroy(GameObject.Find(totalColliderName));
+                        iconInteraction.gameObject.SetActive(false);
+                        scripts.inventory.AddItem(totalColliderName);
+                        break;
+                    case "location":
+                        scripts.locations.ActivateLocation(totalColliderName, spawnName);
+                        break;
+                    default:
+                        if (!scripts.dialogsManager.dialogMenu.activeSelf)
+                            scripts.dialogsManager.ActivateDialog(totalColliderName);
+                        break;
+                }
 
-            if (GameObject.Find(totalColliderName).GetComponent<extraInteraction>())
-            {
-                extraInteraction EI = GameObject.Find(totalColliderName).GetComponent<extraInteraction>();
-                if (EI.NextStep && EI.stageInter == scripts.quests.totalStep)
-                    scripts.quests.NextStep();
-                if (EI.swapPlayerVisual)
-                    scripts.player.ChangeVisual(EI.playerVisual);
-                if (EI.destroyAfterInter)
-                    Destroy(GameObject.Find(totalColliderName));
+                if (GameObject.Find(totalColliderName).GetComponent<extraInteraction>())
+                {
+                    extraInteraction EI = GameObject.Find(totalColliderName).GetComponent<extraInteraction>();
+                    if (EI.NextStep && EI.stageInter == scripts.quests.totalStep)
+                        scripts.quests.NextStep();
+                    if (EI.swapPlayerVisual)
+                        scripts.player.ChangeVisual(EI.playerVisual);
+                    if (EI.destroyAfterInter)
+                        Destroy(GameObject.Find(totalColliderName));
+                }
             }
         }
 
--- manageLocation.cs
+++ manageLocation.cs
@@ -6,12 +6,17 @@ using DG.Tweening;
 
 public class manageLocation : MonoBehaviour
 {
+    public location totalLocation;
     public location[] locations = new location[0];
     [SerializeField] private Image noViewPanel;
     [SerializeField] private allScripts scripts;
     private GameObject player;
 
-    private void Start() => player = GameObject.Find("Player");
+    private void Start()
+    {
+        totalLocation = GetLocation("mainMark");
+        player = GameObject.Find("Player");
+    }
 
     public void ActivateLocation(string name, string spawn)
     {
@@ -21,6 +26,7 @@ public class manageLocation : MonoBehaviour
             {
                 if (!location.locked)
                 {
+                    totalLocation = location;
                     Sequence sequence = DOTween.Sequence();
                     Tween fadeAnimation = noViewPanel.DOFade(100f, 0.5f).SetEase(Ease.InQuart);
                     fadeAnimation.OnComplete(() =>
@@ -36,12 +42,23 @@ public class manageLocation : MonoBehaviour
             }
         }
     }
+
+    public location GetLocation(string name)
+    {
+        foreach (location location in locations)
+        {
+            if (location.name == name)
+                return location;
+        }
+        return null;
+    }
 }
+
 
 [System.Serializable]
 public class location
 {
     public string name;
-    public bool locked;
+    public bool locked, autoEnter;
     public Transform[] spawns; // 0 - left, 1 - right, 2 - up, 3 - down
 }
